e19adffd3290c8e6ba6aa07f01aa2fa37bd440e0
GoPotato <70549598+GoPotatos@users.noreply.github.com>
2021-03-03 01:37:26 +0200

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -72,7 +72,7 @@ print_isbn=normalize(doc.xpath("/html/body/div[2]/main/div[3]/div/div[2]/div[1]/
 etext_isbn=normalize(doc.xpath("/html/body/div[2]/main/div[3]/div/div[2]/div[1]/div/div[2]/ul/li[3]/h2/text()")[0])
 raw_edition=doc.xpath("/html/body/div[2]/main/div[3]/div/div[2]/div[1]/div/div[2]/ul/li[4]/text()")[0]
 try:
-	raw_edition	.index("Edition")
+	raw_edition.index("Edition")
 	edition=normalize(raw_edition)
 except:
 	edition=""
