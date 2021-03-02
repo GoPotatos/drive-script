07d9e3179247b4700c2ce52ae8d7ee3ff023e8ad
GoPotato <70549598+GoPotatos@users.noreply.github.com>
2021-03-02 21:27:42 +0200

    Add pdftk

--- pdf.py
+++ pdf.py
@@ -1,22 +1,21 @@
 pages_number=45
-folder1=r'D:/Ullman/pdf-splitter/'
-folder2=r'D:/Ullman/pdf-splitter/split/'
+folder1=r'C:/Users/user/Documents/split/before/'
+folder2=r'C:/Users/user/Documents/split/after/'
 id=""
 json_file_location="credentials.json"
-from PyPDF2 import PdfFileReader as reader, PdfFileWriter as writer
 import os.path
 import os
 import re
 import sys
+import subprocess
 from time import sleep
 from pydrive.drive import GoogleDrive
 from pydrive.auth import GoogleAuth ,ServiceAccountCredentials
 proxy = 'http://localhost:8080'
-
-#os.environ['http_proxy'] = proxy 
-#os.environ['HTTP_PROXY'] = proxy
-#os.environ['https_proxy'] = proxy
-#os.environ['HTTPS_PROXY'] = proxy
+os.environ['http_proxy'] = proxy 
+os.environ['HTTP_PROXY'] = proxy
+os.environ['https_proxy'] = proxy
+os.environ['HTTPS_PROXY'] = proxy
 flag=sys.argv[1]
 gauth = GoogleAuth()
 scope = ['https://www.googleapis.com/auth/drive']
@@ -24,23 +23,17 @@ gauth.credentials = ServiceAccountCredentials.from_json_keyfile_name(json_file_l
 drive = GoogleDrive(gauth)
 
 list1=os.listdir(folder1)
-if flag=="2":
-	#print("Flag")
-	pdf_list=[x for x in list1 if re.search(".\.PDF$",x,re.IGNORECASE)]
-	for x in pdf_list:
+
+def uploadFiles(files_list):
+	for x in files_list:
 		output_name=x
-		output_path=os.path.join(folder1,output_name)
+		output_path=folder1+output_name
 		split_output_name="Split_"+output_name
-		split_path=os.path.join(folder2,split_output_name)
-		print("Directory updated "+output_path)
-		pdf=reader(output_path)
-		outputWriter=writer()
-		for x in range(pages_number):
-			outputWriter.addPage(pdf.getPage(x))
+		split_path=folder2+split_output_name
+		print("Directory updated "+split_path)
+		pages_range="1-"+str(pages_number)
+		subprocess.check_call(["cmd","/c","pdftk",output_path,"cat",pages_range,"output",split_path])
 			
-		with open(split_path,"wb")as outputPDF:		
-			outputWriter.write(outputPDF)
-			outputPDF.close()
 
 		file2 = drive.CreateFile({'title': split_output_name, 
 			"parents":  [{"id": id}], 
@@ -49,6 +42,11 @@ if flag=="2":
 		file2.SetContentFile(split_path)
 		file2.Upload()
 		print(split_output_name+" Successfully Uploaded")
+
+if flag=="2":
+	#print("Flag")
+	pdf_list=[x for x in list1 if re.search(".\.PDF$",x,re.IGNORECASE)]
+	uploadFiles(pdf_list)
 	sys.exit()
 
 
@@ -60,26 +58,5 @@ while True:
 	list1=os.listdir(folder1)
 	
 	if len(unique):
-		for x in unique:
-			output_name=x
-			output_path=os.path.join(folder1,output_name)
-			split_output_name="Split_"+output_name
-			split_path=os.path.join(folder2,split_output_name)
-			print("Directory updated "+output_path)
-			pdf=reader(output_path)
-			outputWriter=writer()
-			for x in range(pages_number):
-				outputWriter.addPage(pdf.getPage(x))
-				
-			with open(split_path,"wb")as outputPDF:		
-				outputWriter.write(outputPDF)
-				outputPDF.close()
-
-			file2 = drive.CreateFile({'title': split_output_name, 
-				"parents":  [{"id": id}], 
-				"mimeType": "application/pdf"})
-
-			file2.SetContentFile(split_path)
-			file2.Upload()
-			print(" Successfully Uploaded",split_output_name)
+		uploadFiles(unique)
 	sleep(2)
